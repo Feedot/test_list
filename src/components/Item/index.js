144a9fde87ecf09e3f0ff0f16245e57b65ff72a3
1 <a1@MacBook-Air-1.local>
2018-03-25 09:43:07 +0300

    sec

--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 const Item = (props) =>{
-    const {data,deleteItem,active,toogleItem,num} = props,
-        {id,name,number} = data;
+    const {data,deleteItem,active,toogleItem,num} = props, {id,name,number} = data;
+
     return(
         <div onClick={() => toogleItem(num)}
              className={ active? "item_wrapper active" : "item_wrapper"}>
