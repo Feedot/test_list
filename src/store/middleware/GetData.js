19d1405ce8a1eb0743cf16e2751364d87562292e
1 <a1@MacBook-Air-1.local>
2018-03-26 14:02:30 +0300

    w

--- src/store/middleware/GetData.js
+++ src/store/middleware/GetData.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export default ({ dispatch }) => next => ({ type, payload }) => {
     (type === "TAKE_DATA") ?
-        axios.get("data.json").then(({ data, ...rest }) =>  dispatch({ type: "GET_DATA", payload: data }) ):
+        axios.get("data.json").then(({ data }) =>  dispatch({ type: "GET_DATA", payload: data }) ):
         next({ type, payload });
 };
